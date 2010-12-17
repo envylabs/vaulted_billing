4f4681cef74ab179094f7b7137e3ff62f505ff2b
Nathaniel Bibler <nate@envylabs.com>
2010-12-17 08:14:43 -0500

    Fixed deprecation warning for VCR

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -1,6 +1,6 @@
 VCR.config do |config|
   config.cassette_library_dir = File.expand_path(File.join(File.dirname(__FILE__), '..', 'fixtures', 'net'))
-  config.http_stubbing_library = :webmock
+  config.stub_with :webmock
   config.ignore_localhost = true
   config.default_cassette_options = {
     :record => :none
