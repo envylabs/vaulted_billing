47ce48aecec46968d74feb6e69fea31f4331441e
Nathaniel Bibler <nate@envylabs.com>
2010-12-17 08:27:54 -0500

    Removed unused freeze_time method

--- spec/support/helpers.rb
+++ spec/support/helpers.rb
@@ -1,9 +1,5 @@
 module RSpecHelpers
   module InstanceMethods
-    def freeze_time(time=nil)
-      (time ||= Time.now).tap { |now| Time.stub(:now).and_return(now) }
-    end
-
     shared_examples_for "a transaction request" do
       it 'returns a Transaction' do
         subject.should be_kind_of(VaultedBilling::Transaction)
