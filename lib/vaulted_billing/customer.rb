017df41a50706de4040ff260bc27488bf454ce8c
Nathaniel Bibler <nbibler@nathanielbibler.com>
2010-12-25 18:37:02 -0800

    Added Customer documentation.

--- lib/vaulted_billing/customer.rb
+++ lib/vaulted_billing/customer.rb
@@ -1,9 +1,24 @@
 module VaultedBilling
+  ##
+  # An intermediary object for VaultedBilling which represents a single
+  # customer on the gateway.
+  #
+  # Many gateways support you in defining multiple credit cards or payment
+  # methods under a single customer object.  To support this, they will
+  # generate one identifier for the top-level customer, and then separate
+  # identifiers for each payment method (see VaultedBilling::CreditCard).
+  #
   class Customer
-    attr_accessor :vault_id
-    attr_accessor :merchant_id
-    attr_accessor :email
+    attr_accessor :email # Optional email address for the customer.
+    attr_accessor :merchant_id # Optional custom identifier for the customer (i.e. your database key).
+    attr_accessor :vault_id # Gateway generated unique identifier for this customer in their system.
 
+    ##
+    # You can mass assign the attributes by passing a hash with keys
+    # matching attributes of the Customer:
+    #
+    #     Customer.new(:merchant_id => 1)
+    #
     def initialize(attributes = {})
       attributes = HashWithIndifferentAccess.new(attributes)
       @vault_id = attributes[:vault_id]
