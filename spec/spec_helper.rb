174043a53ce57dbc26c5b29bdd4f47dbcc868591
Nathaniel Bibler <nate@envylabs.com>
2011-08-10 11:32:38 -0400

    Allow spec focusing.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,6 @@
 $:.unshift(File.expand_path('../../lib', __FILE__))
 require 'rubygems'
-require 'bundler'
-Bundler.setup
+require 'bundler/setup'
 Bundler.require :default, :test
 
 require 'vaulted_billing'
@@ -9,4 +8,10 @@ require 'rspec'
 
 Dir[File.join(File.dirname(__FILE__), 'support', '**', '*.rb')].each do |file|
   require file
+end
+
+RSpec.configure do |config|
+  config.mock_with :rspec
+  config.filter_run :focus => true
+  config.run_all_when_everything_filtered = true
 end
